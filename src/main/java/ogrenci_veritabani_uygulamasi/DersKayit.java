17d080da92d3cc0611b9c7f735fc5af2356e367e
sefasezr <153550790+sefasezr@users.noreply.github.com>
2024-06-12 16:35:35 +0300

    ...

--- src/main/java/ogrenci_veritabani_uygulamasi/DersKayit.java
+++ src/main/java/ogrenci_veritabani_uygulamasi/DersKayit.java
@@ -78,7 +78,6 @@ public class DersKayit extends OgrenciKayit {
                     break;
                 default:
                     System.out.println("Geçersiz işlem yaptınız");
-                    dersKaydi();
 
             }
         }while(devam);
